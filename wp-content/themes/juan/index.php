a019f5d5d055e6e734eb8a9acf265e391471f98f
santo52 <sanruiz1003@gmail.com>
2016-09-18 00:25:09 -0500

    Template sass Configurations

--- wp-content/themes/juan/index.php
+++ wp-content/themes/juan/index.php
@@ -1,21 +1,12 @@
 <?php get_header(); ?>
-<?php global $post;
-$my_query = new WP_Query('category_name=noticias&showposts=10'); ?>
-    <ul class="News row ">
-
-        <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-            <?php
-            $back = get_post_custom_values('fondo', $post->ID)[0];
-            $color = get_post_custom_values('color', $post->ID)[0];
-            ?>
-            <li class="col-4 cols-12 row middle end" style='background : <?php echo $back ?>'>
-                <a href=" <?php echo get_permalink($post->ID) ?>">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <?php the_post_thumbnail(); ?>
-                    <?php else: echo '<h2 class="end" style="color:  ' . $color . ' ">' . get_the_title() . '</h2> <span style="color: ' . $color . '"> + </span>' ?>
-                    <?php endif; ?>
-                </a>
-            </li>
-        <?php endwhile; ?>
-    </ul>
+<section class="SliderIndex">
+    <blockquote class="row middle">
+        <p>
+            SERVICIOS Y SOLUCIONES <br>
+            <sup>EN</sup> GOBIERNO CORPORATIVO <br>
+            <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
+            <span style="font-size: 2.75rem">A SU ALCANCE</span>
+        </p>
+    </blockquote>
+</section>
 <?php get_footer(); ?>
