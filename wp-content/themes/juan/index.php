1fe2f37d50cc1eb56b8f37b366fc416b5751e871
santo52 <sanruiz1003@gmail.com>
2016-09-18 09:39:55 -0500

    Slider

--- wp-content/themes/juan/index.php
+++ wp-content/themes/juan/index.php
@@ -1,12 +1,66 @@
 <?php get_header(); ?>
 <section class="SliderIndex">
-    <blockquote class="row middle">
-        <p>
-            SERVICIOS Y SOLUCIONES <br>
-            <sup>EN</sup> GOBIERNO CORPORATIVO <br>
-            <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
-            <span style="font-size: 2.75rem">A SU ALCANCE</span>
-        </p>
-    </blockquote>
+    <section class="Slider-images" style="width:400%">
+        <article class="Item" style="width:25%">
+            <blockquote class="row middle color-darkBlue">
+                <p>
+                    SERVICIOS Y SOLUCIONES <br>
+                    <sup>EN</sup> <span style="font-size: 1.45rem">GOBIERNO CORPORATIVO</span> <br>
+                    <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
+                    <span style="font-size: 2.75rem">A SU ALCANCE</span>
+                </p>
+            </blockquote>
+            <img src="<?php bloginfo('template_url') ?>/assets/slider1/slider_01.jpg" alt="">
+        </article>
+        <article class="Item" style="width:25%">
+            <blockquote class="row middle color-darkBlue">
+                <p>
+                    SERVICIOS Y SOLUCIONES <br>
+                    <sup>EN</sup> <span style="font-size: 1.45rem">GOBIERNO CORPORATIVO</span> <br>
+                    <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
+                    <span style="font-size: 2.75rem">A SU ALCANCE</span>
+                </p>
+            </blockquote>
+            <img src="<?php bloginfo('template_url') ?>/assets/slider1/slider_01.jpg" alt="">
+        </article>
+        <article class="Item" style="width:25%">
+            <blockquote class="row middle color-darkBlue">
+                <p>
+                    SERVICIOS Y SOLUCIONES <br>
+                    <sup>EN</sup> <span style="font-size: 1.45rem">GOBIERNO CORPORATIVO</span> <br>
+                    <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
+                    <span style="font-size: 2.75rem">A SU ALCANCE</span>
+                </p>
+            </blockquote>
+            <img src="<?php bloginfo('template_url') ?>/assets/slider1/slider_01.jpg" alt="">
+        </article>
+        <article class="Item" style="width:25%">
+            <blockquote class="row middle color-darkBlue">
+                <p>
+                    SERVICIOS Y SOLUCIONES <br>
+                    <sup>EN</sup> <span style="font-size: 1.45rem">GOBIERNO CORPORATIVO</span> <br>
+                    <sup>Y</sup> <span style="font-size: 1.28rem"> ARQUITECTURA EMPRESARIAL</span><br>
+                    <span style="font-size: 2.75rem">A SU ALCANCE</span>
+                </p>
+            </blockquote>
+            <img src="<?php bloginfo('template_url') ?>/assets/slider1/slider_01.jpg" alt="">
+        </article>
+    </section>
+    <div class="Slider-arrows">
+        <span class="Arrow Arrow--prev">
+            <img src="<?php bloginfo('template_url') ?>/assets/images/arrow.svg" alt="arrow">
+        </span>
+        <span class="Arrow Arrow--next">
+            <img src="<?php bloginfo('template_url') ?>/assets/images/arrow.svg" alt="arrow">
+        </span>
+    </div>
+    <div class="Slider-circles">
+        <div class="Circles-container">
+            <span class="Item active"></span>
+            <span class="Item"></span>
+            <span class="Item"></span>
+            <span class="Item"></span>
+        </div>
+    </div>
 </section>
 <?php get_footer(); ?>
