66778df24223b22a2de603aefe64cd7bb5b2f586
santo52 <sanruiz1003@gmail.com>
2016-11-11 15:59:02 -0500

    shortcodes
    a�adir el siguiente códico al home : [swiper id=1][/swiper][swiper id=2][/swiper][swiper id=3][/swiper][swiper id=4 size=small][/swiper]

--- wp-content/plugins/swiper/swiper.php
+++ wp-content/plugins/swiper/swiper.php
@@ -21,6 +21,7 @@ require_once "core/ShortCodes.php";
 /*** ADD MENU ***/
 
 function swiper_options_panel() {
+    
     add_menu_page('Sliders', 'Sliders', 'manage_options', plugin_dir_path(__FILE__) . 'app/index.php' , null);
     add_submenu_page( plugin_dir_path(__FILE__) . 'app/index.php', 'Nuevo', 'Nuevo', 'manage_options', plugin_dir_path(__FILE__) . 'app/elements/new.php', null);
     add_submenu_page( null, 'Update', 'Update', 'manage_options', plugin_dir_path(__FILE__) . 'app/elements/edit.php', null);
