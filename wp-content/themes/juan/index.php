246ed10188c6193bc2374e6250863dd6933504ec
santo52 <sanruiz1003@gmail.com>
2016-11-11 14:36:32 -0500

    addlink

--- wp-content/themes/juan/index.php
+++ wp-content/themes/juan/index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <?php echo swiper_function(1); ?>
 <?php echo swiper_function(2); ?>
+<a href="#">
 <?php echo swiper_function(3); ?>
+</a>
 <?php echo swiper_function(4, 'small'); ?>
 <?php get_footer();?>
