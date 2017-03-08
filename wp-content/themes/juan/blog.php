04fe6fc370f85a8f937f5252e875f0cdafd022d0
Juan2Ramos <juan2ramos@gmail.com>
2017-03-08 10:41:30 -0500

    blog https

--- wp-content/themes/juan/blog.php
+++ wp-content/themes/juan/blog.php
@@ -6,7 +6,7 @@ Template Name: Blog
 <?php get_header(); ?>
 
 <?php global $post;
-$my_query = new WP_Query('category_name=blog&showposts=100'); ?>
+$my_query = new WP_Query('category_name=gobierno-corporativo&showposts=100'); ?>
 <section class="row Half center stretch" id="blog">
     <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
 
@@ -23,5 +23,6 @@ $my_query = new WP_Query('category_name=blog&showposts=100'); ?>
     <?php endwhile;
     wp_reset_postdata(); ?>
 </section>
+asdasdasd
 <?php get_footer(); ?>
 
---
+++ wp-content/themes/juan/category-gestion-de-servicios.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Blog
+*/
+?>
+<?php get_header(); ?>
+
+<?php global $post;
+$my_query = new WP_Query('category_name=gestion-de-servicios&showposts=100'); ?>
+<div class="row  cols-12" id="blog">
+    <section class="row Half center stretch col-8 cols-12" >
+        <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+            <article class="col-12 cols-12  row start">
+                <div class="col-9 cols-12">
+                    <h2><?php the_title() ?></h2>
+                    <?php the_excerpt() ?>
+                </div>
+                <figure class="col-3 cols-12">
+                    <?php the_post_thumbnail(); ?>
+                </figure>
+            </article>
+
+        <?php endwhile;wp_reset_postdata(); ?>
+    </section>
+    <aside class=" col-3">
+        <div class="Content-aside">
+            <h3>Todos los articulos</h3>
+            <?php $my_query1 = new WP_Query('category_name=gestion-de-servicios&showposts=100'); ?>
+            <?php while ($my_query1->have_posts()) : $my_query1->the_post(); ?>
+                <ul>
+
+                    <li><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></li>
+                </ul>
+            <?php endwhile; wp_reset_postdata(); ?>
+        </div>
+    </aside>
+</div>
+<?php get_footer(); ?>
+
